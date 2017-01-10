f8741d31677a2d31ef70c3cc4804ccdb73ea2aa7
Yana Melnik <melnik.yana.victorovna@gmail.com>
2017-01-10 22:09:57 +0200

    slider ready

--- script/slider.js
+++ script/slider.js
@@ -2,8 +2,6 @@ $('.slider').slick({
     autoplay: true,
     autoplaySpeed: 1500,
     speed: 1200,
-    centerMode: true,
-    centerPadding: '10px',
     dots: true,
     arrows: false,
     pauseOnDotsHover: true
