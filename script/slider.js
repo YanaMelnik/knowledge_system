b2496d5d8a59adbe6559f5e66973f918f60df8e2
Yana Melnik <melnik.yana.victorovna@gmail.com>
2017-01-10 15:30:17 +0200

    Slider works There is the form of enter

---
+++ script/slider.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 1200,
+    centerMode: true,
+    centerPadding: '10px',
+    dots: true,
+    arrows: false,
+    pauseOnDotsHover: true
+
+});
