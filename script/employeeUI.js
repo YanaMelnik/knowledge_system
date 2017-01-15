aacc14a71dd346e4654e88113d30dd13dd31ffae
Yana Melnik <melnik.yana.victorovna@gmail.com>
2017-01-15 15:19:27 +0200

    employee general page

--- script/employeeUI.js
+++ script/employeeUI.js
@@ -1,7 +1,10 @@
 function EmployeeComponent() {
     var container = $('#container');
     var employee = $('#employeeComponent');
+
     container.html(employee.html());
 
+    var general= $('#generalEmployee');
+    container.find('.container').html(general.html());
 
 }
