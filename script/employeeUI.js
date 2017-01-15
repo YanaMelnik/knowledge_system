535129795da0aed798f8de2c75178dfbd46d6336
Yana Melnik <melnik.yana.victorovna@gmail.com>
2017-01-15 15:50:04 +0200

    add mustache

--- script/employeeUI.js
+++ script/employeeUI.js
@@ -7,4 +7,22 @@ function EmployeeComponent() {
     var general= $('#generalEmployee');
     container.find('.container').html(general.html());
 
+    $.ajax({
+        type: 'GET',
+        url: '/employee/1',
+        data: JSON.stringify(loginData),
+        success: function (data) {
+            ;
+        },
+        error: function () {
+            alert('Error');
+        },
+        dataType: 'json',
+        contentType: 'application/json'
+    });
+
+    function render(data) {
+        var photo = container.find();
+    }
+
 }
